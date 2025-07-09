8a974f1b5cea325258d01746bcd09e8524287772
Lucas Rodrigues <lucas.rodrigues@clickbus.com>
2025-07-09 18:06:50 -0300

    Startup project

---
+++ src/test/java/com/test/vr/VrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.vr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
